e92bb18ce40a9496ebb0a3abb22de780db1eb66e
hari27om <hariomtri27@gmail.com>
2023-08-29 20:52:29 +0530

    Updated Signup page

--- mernapp/src/screens/Signup.jsx
+++ mernapp/src/screens/Signup.jsx
@@ -1,68 +1,112 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import Navbar from '../components/Navbar';
+
 
 const Signup = () => {
-  const [credentials, setcredentials] = useState({ name: "", email: "", password: "", geolocation: "" })
-  const handleSubmit = async (e,res) => {
-    try {
-    e.preventDefault()
-    const response = await fetch("http://localhost:5000/api/createuser", {
+  const [credentials, setCredentials] = useState({ name: "", email: "", password: "", geolocation: "" })
+  let [address, setAddress] = useState("");
+  let navigate = useNavigate()
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    let navLocation = () => {
+      return new Promise((res, rej) => {
+        navigator.geolocation.getCurrentPosition(res, rej);
+      });
+    }
+    let latlong = await navLocation().then(res => {
+      let latitude = res.coords.latitude;
+      let longitude = res.coords.longitude;
+      return [latitude, longitude]
+    })
+    // console.log(latlong)
+    let [lat, long] = latlong
+    console.log(lat, long)
+    const response = await fetch("http://localhost:5000/api/auth/getlocation", {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({ name: credentials.name, email: credentials.email, password: credentials.password, location: credentials.geolocation })
-    })
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
+      body: JSON.stringify({ latlong: { lat, long } })
+
+    });
+    const { location } = await response.json()
+    console.log(location);
+    setAddress(location);
+    setCredentials({ ...credentials, [e.target.name]: location })
+  }
+
+
+  const handleSubmit = async (e, res) => {
+    try {
+      e.preventDefault()
+      const response = await fetch("http://localhost:5000/api/createuser", {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ name: credentials.name, email: credentials.email, password: credentials.password, location: credentials.geolocation })
+      })
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      const json = await response.json();
+      console.log(json);
+      if (json.success) {
+        //save the auth toke to local storage and redirect
+        localStorage.setItem('token', json.authToken)
+        navigate("/login")
+  
+      }
+      else {
+        alert("Enter Valid Credentials")
+      }
     }
-    const json = await response.json();
-    console.log(json);
-    if (!json.success) {
-      alert("Enter Valid Credentials")
+    catch (error) {
+      console.error('Error:', error);
+      // Handle the error, e.g., display an error message to the user
     }
   }
-  catch (error) {
-    console.error('Error:', error);
-    // Handle the error, e.g., display an error message to the user
-  }
-}
 
 
   const onChange = (event) => {
-    setcredentials({ ...credentials, [event.target.name]: event.target.value })
+    setCredentials({ ...credentials, [event.target.name]: event.target.value })
   }
 
   return (
-    <>
-      <div className='container'>
-        < form onSubmit={handleSubmit}>
-          <div className="mb-3">
-            <label htmlFor="name" className="form-label">Name</label>
-            <input type="text" className="form-control" name='name' value={credentials.name} onChange={onChange} />
-          </div>
-
-          <div className="mb-3">
-            <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name='email' value={credentials.email} onChange={onChange} />
-            <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-          </div>
-
-          <div className="mb-3">
-            <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-            <input type="password" className="form-control" id="exampleInputPassword1" name='password' value={credentials.password} onChange={onChange} />
-          </div>
-
-          <div className="mb-3">
-            <label htmlFor="location" className="form-label">Location</label>
-            <input type="text" className="form-control" name='geolocation' value={credentials.geolocation} onChange={onChange} />
-          </div>
-
-          <button type="submit" className="btn btn-primary">Submit</button>
-          <Link to="/login" className="m-3 btn btn-danger">Already a user?</Link>
-        </form>
+    <div>
+      <div>
+        <Navbar></Navbar>
       </div>
-    </>
+      <div className='container' >
+          <form className='w-50 m-auto mt-5 border bg-dark border-success rounded' onSubmit={handleSubmit}>
+            <div className="m-3">
+              <label htmlFor="name" className="form-label">Name</label>
+              <input type="text" className="form-control" name='name' value={credentials.name} onChange={onChange} aria-describedby="emailHelp" />
+            </div>
+            <div className="m-3">
+              <label htmlFor="email" className="form-label">Email address</label>
+              <input type="email" className="form-control" name='email' value={credentials.email} onChange={onChange} aria-describedby="emailHelp" />
+            </div>
+            <div className="m-3">
+              <label htmlFor="address" className="form-label">Address</label>
+              <fieldset>
+                <input type="text" className="form-control" name='address' placeholder='"Click below for fetching address"' value={address} onChange={(e)=>setAddress(e.target.value)} aria-describedby="emailHelp" />
+              </fieldset>
+            </div>
+            <div className="m-3">
+              <button type="button" onClick={handleClick} name="geolocation" className=" btn btn-success">Click for current Location </button>
+            </div>
+            <div className="m-3">
+              <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+              <input type="password" className="form-control" value={credentials.password} onChange={onChange} name='password' />
+            </div>
+            <button type="submit" className="m-3 btn btn-success">Submit</button>
+            <Link to="/login" className="m-3 mx-1 btn btn-danger">Already a user</Link>
+          </form>
+        </div>
+      </div>
   )
 }
 
